8b2fa642b7553ecd456efcd2a65f4f33ca10cdb2
Lukinos <LukinoProg@gmail.com>
2016-08-24 17:37:45 +0200

    Add gulp task to execute webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,40 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const shell = require('gulp-shell');
 
+
+// var browserSync = require('browser-sync').create();
+//
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./src",
-            routes: {
-                "/node_modules": "node_modules"
-            }
-        }
-    });
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./src",
+//             routes: {
+//                 "/node_modules": "node_modules"
+//             }
+//         }
+//     });
+// });
+
+
+gulp.task('server:dev', shell.task([
+    'webpack-dev-server --config ./webpack.config.js --progress --watch'
+]));
+
+
+// Don't work for the moment... Spam in console
+gulp.task('server:dev:dashboard', shell.task([
+    'webpack-dev-server --config ./webpack.config.js --progress --watch'
+]));
+
+
+gulp.task('server:prod', function() {
+
+});
+
+
+gulp.task('build', function() {
+
 });
 
 // or...
